f99f22a242b708686d50e3ac9c70e97e64f4baab
folawer21 <folawer21@mail.ru>
2024-01-10 01:24:14 +0300

    done

---
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Александр  Сухинин on 07.02.2024.
+//
+
+import Foundation
+import UIKit
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var profileNickName: UILabel!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileDescription: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
