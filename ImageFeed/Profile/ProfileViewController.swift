41e6e41e7cb7145f080b01cceb59ccff05c4b0f3
folawer21 <folawer21@mail.ru>
2024-02-18 00:28:02 +0300

    done

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -3,18 +3,70 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    @IBOutlet weak var profilePhoto: UIImageView!
-    @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var profileNickName: UILabel!
-    @IBOutlet weak var profileName: UILabel!
-    @IBOutlet weak var profileDescription: UILabel!
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let profileImageView = UIImageView()
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.image = UIImage(named: "ekaterina")
+        view.addSubview(profileImageView)
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)
+        ])
+        
+        let nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = UIColor(named: "YPWhite")
+        nameLabel.font = UIFont(name: "System Font Regular", size: 23.0)
+        view.addSubview(nameLabel)
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor , constant: -15)
+        ])
+        
+        let nicknameLabel = UILabel()
+        nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nicknameLabel.textColor = UIColor(named: "YPGray")
+        nicknameLabel.text = "@ekaterina_nov"
+        nicknameLabel.font = UIFont(name: "System Font Regular", size: 13)
+        view.addSubview(nicknameLabel)
+        NSLayoutConstraint.activate([
+            nicknameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            nicknameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            nicknameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        ])
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.textColor  = UIColor(named: "YPWhite")
+        descriptionLabel.text = "Hellow, world!"
+        descriptionLabel.font = UIFont(name: "System Font Regular", size: 13)
+        view.addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        ])
+
+        let exitButton = UIButton.systemButton(with: UIImage(named: "exitButton")!, target: self, action: nil)
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        exitButton.tintColor = UIColor(named: "YPRed")
+        view.addSubview(exitButton)
+        NSLayoutConstraint.activate([
+            exitButton.widthAnchor.constraint(equalToConstant: 24),
+            exitButton.heightAnchor.constraint(equalToConstant: 24),
+            exitButton.leadingAnchor.constraint(greaterThanOrEqualTo: profileImageView.trailingAnchor),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -24),
+            exitButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55)
+        ])
     }
 }
 
