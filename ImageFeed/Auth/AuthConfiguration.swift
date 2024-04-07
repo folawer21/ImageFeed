08f83f1332a7e448253990917727b36693059d80
folawer21 <folawer21@mail.ru>
2024-04-08 00:50:48 +0300

    2222

--- ImageFeed/Auth/AuthConfiguration.swift
+++ ImageFeed/Auth/AuthConfiguration.swift
@@ -8,12 +8,27 @@
 import Foundation
 
 enum Constants{
-    static let accessKey = "47C4Zds9l356Sr0wc1FBik0KWAXiDP933YVV_FJOfrI";
-    static let secretKey = "mBE8WA1zIEGZrXl6nABrjj3fNXfGvy7xdZBLsyLe_dY";
+    static let accessKey = "W222z7fyUGHqSCByRZJPutRfREuVa50J5RJFbhexn7Q";
+    static let secretKey = "OmU7u-wv8iDsf90zAUJ4Q7JCIwiAsA7OoZMbqFrAV3M";
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob";
     static let accessScope = "public+read_user+write_likes";
     static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
     static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+//    
+
+//    static let accessKey = "Q-pM9VMhOzOVE_l35jdcwC2HopBklHme4fiGB5f7mCo";
+//    static let secretKey = "PzY6cBpeRA6MSCjLUESIc8YBql-nM8EDL_z7WDuv5kY";
+//    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob";
+//    static let accessScope = "public+read_user+write_likes";
+//    static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+//    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    
+//    static let accessKey = "47C4Zds9l356Sr0wc1FBik0KWAXiDP933YVV_FJOfrI";
+//    static let secretKey = "mBE8WA1zIEGZrXl6nABrjj3fNXfGvy7xdZBLsyLe_dY";
+//    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob";
+//    static let accessScope = "public+read_user+write_likes";
+//    static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+//    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
 
 struct AuthConfiguration{
